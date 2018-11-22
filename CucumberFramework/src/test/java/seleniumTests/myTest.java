87cd3f88127f4cd10f07f32a3a3e11b656f3b3f9
aravindaswamydv <aravindaswamydv@gmail.com>
2018-11-22 12:12:20 +0530

    aravinda commit 11/22

---
+++ CucumberFramework/src/test/java/seleniumTests/myTest.java
@@ -0,0 +1,14 @@
+package seleniumTests;
+
+public class myTest {
+
+	public myTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
